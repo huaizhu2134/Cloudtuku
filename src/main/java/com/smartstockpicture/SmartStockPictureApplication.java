ac3202c96109049754ab004139c680f265a85202
dongfanghuaizhu <2134769644@qq.com>
2026-05-30 15:58:31 +0800

    初始

---
+++ src/main/java/com/smartstockpicture/SmartStockPictureApplication.java
@@ -0,0 +1,13 @@
+package com.smartstockpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStockPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStockPictureApplication.class, args);
+    }
+
+}
